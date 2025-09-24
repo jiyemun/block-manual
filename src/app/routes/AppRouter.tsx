8d4dde436ba1f43e8fa24fb3e73c3933702561f1
jiyemun <jym@inca.co.kr>
2025-09-24 16:57:00 +0900

    router update

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import BaseLayout from "@/widgets/layout/BaseLayout.tsx";
 import ManualEditor from "@/pages/manual-editor/ui/ManualEditor.tsx";
 import ManualList from "@/pages/manual-list/ui/ManualList.tsx";
@@ -6,12 +6,11 @@ import ManualList from "@/pages/manual-list/ui/ManualList.tsx";
 const AppRouter = () => {
   return (
     <Routes>
-      {/* 일반 페이지 - BaseLayout 사용 */}
       <Route path="/" element={<BaseLayout />}>
         <Route index element={<ManualList />} />
         <Route path="editor" element={<ManualEditor />} />
       </Route>
-      {/*<Route path="*" element={<Navigate to="/" replace />} />*/}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
