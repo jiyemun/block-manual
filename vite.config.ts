50e1cf411de9c65ebf11da1d047c8fbc85b36c3a
jiyemun <jym@inca.co.kr>
2025-09-25 13:55:14 +0900

    federation setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: "block-manual",
+      filename: "blockManual.js",
+      exposes: {
+        "./blockManual": "./src/app/entrypoint/App.tsx",
+      },
+      shared: [
+        "react",
+        "react-dom",
+        "react-router-dom",
+        "antd",
+        "@ant-design/pro-components",
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
